f5e447f47d0f05c0572c9faa844cd8e6fab66491
saltapi10 <jsaltapi@ibhellas.gr>
2024-11-14 19:08:41 +0200

    login register labels different ids, header data global

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 
 class DashboardController extends Controller
 {
     public function view()
     {
         $user = Auth::user();
+
+        $headerData = [
+            'title' => 'Movie World'
+        ];
+        View::share('header_data', $headerData);
 
         return view('dashboard', [
             'sessionExists' => (bool)$user,
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,12 @@
 <form method="POST" action="/login" class="text-dark">
     @csrf
     <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input required type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <label for="login_email" class="form-label">Email address</label>
+        <input required type="email" name="email" class="form-control" id="login_email">
     </div>
     <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input required type="password" name="password" class="form-control" id="exampleInputPassword1">
+        <label for="login_password" class="form-label">Password</label>
+        <input required type="password" name="password" class="form-control" id="login_password">
     </div>
     <button type="submit" class="btn btn-primary mb-3">Submit</button>
 </form>
